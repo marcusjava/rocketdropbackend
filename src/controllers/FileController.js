72f7c0978612c332b0d7b969fdf9e521717415ca
marcusjava <marcusbuba@hotmail.com>
2019-04-11 09:44:04 -0300

    env configurado

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -3,12 +3,12 @@ const Box = require('../models/Box');
 
 class FileController {
 	async store(req, res) {
-		console.log(req.file);
+		const url = process.env.URL || 'http://localhost:3000/files';
 		const box = await Box.findById(req.params.id);
 		const file = await File.create({
 			title: req.file.originalname,
 			path: req.file.key,
-			url: `http://localhost:3000/files/${req.file.key}`,
+			url: `${url}/files/${req.file.key}`,
 		});
 		box.files.push(file);
 		await box.save();
